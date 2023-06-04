3de8ae76184191b29c042f20f39339eab6c1cde7
Your Name <bekhzod2204@gmail.com>
2023-06-04 18:29:36 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import Main from "./Main";
+
+export default function App() {
+    
+    return (
+        <View style={{ flex: 1 }}>
+            <Main />
+        </View>
+    );
+}
---
+++ Main.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { View, Button } from "react-native";
+import { WebView } from "react-native-webview";
+import { Asset } from "expo-asset";
+import { useAssets } from "expo-asset";
+import { readAsStringAsync } from 'expo-file-system';
+
+
+const Main = () => {
+    const [location, setLocation] = useState(null);
+    const [index, indexLoadingError] = useAssets(require("./map.html"));
+    const mapHtml = Asset.fromModule(require("./map.html")).uri;
+    const [html, setHtml] = useState("");
+
+    if (index) {
+        readAsStringAsync(index[0].localUri).then((data) => {
+            setHtml(data);
+        });
+    }
+
+    const onMessage = (payload) => {
+
+        let dataPayload;
+        try {
+          dataPayload = JSON.parse(payload.nativeEvent.data);
+        } catch (e) {}
+      
+        if (dataPayload) {
+          if (dataPayload.type === 'Console') {
+            console.log(`[Console] ${JSON.stringify(dataPayload.data)}`);
+          } else {
+            setLocation(dataPayload);
+            console.log(dataPayload);
+          }
+        }
+      };
+    
+    return (
+        <View style={{ flex: 1 }}>
+
+            <WebView
+                style={{marginTop: 26}}
+                onLoad={() => {}}
+                source={{ html }}
+                onMessage={onMessage}
+            />
+            <Button
+                title="Log Location"
+                onPress={() => console.log(location)}
+            />
+        </View>
+    );
+}
+
+export default Main;
